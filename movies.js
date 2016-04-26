19f67d987001a9d0b0590dc396df8f3b1a6d0583
Aditya Agarwal <skymanaditya1@gmail.com>
2016-04-26 15:56:54 +0530

    Setting the div tag from within the js

---
+++ movies.js
@@ -0,0 +1,10 @@
+function createMoviesList(){
+	var s;
+	s = "<ul>"
+		+ "<li>Primary Sector</li>"
+		+ "<li>Secondary Sector</li>"
+		+ "<li>Tertiary Sector</l1>"
+		+ "</ul>";
+	divMovies = document.getElementById("divMovies");
+	divMovies.innerHTML = s;
+}
